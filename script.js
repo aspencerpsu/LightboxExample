9f1d8500074056d3eff5a041ab2fc853b23c2a6d
Akeem Spencer <aspencerpsu@gmail.com>
2015-12-11 11:09:09 -0500

    The images and the page was edited so the image is front and center from the opaque

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $(document).redy(function(){
 
 	I need the first page to be opaque when I click on the a href tag
 		(SOLUTION!: I can use jquery to change the style of the backpage;)
+
 	I need the image to be centered
 	I need the front of the image to be front and centered from the opaque page
 	I need the 
